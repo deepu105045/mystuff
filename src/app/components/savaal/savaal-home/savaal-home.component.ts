eeba1e7e3cc14d5a915b5963035549a12d10504d
deepu105045 <deepu105045@gmail.com>
2021-01-13 21:29:13 +0530

    update background path

--- src/app/components/savaal/savaal-home/savaal-home.component.ts
+++ src/app/components/savaal/savaal-home/savaal-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-savaal-home',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./savaal-home.component.css']
 })
 export class SavaalHomeComponent implements OnInit {
+  eventId: string;
 
-  constructor() { }
+  constructor(private _router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  goToEventsPage(){
+    console.log(this.eventId)
+    this._router.navigate([`/savaal/${this.eventId}`])
+
   }
 
 }
